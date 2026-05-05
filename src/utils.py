707fc8e0bf5219f6257e441db0d19d92415b20e8
Gabriele <tomaigabriele@gmail.com>
2026-05-05 16:53:07 +0200

    now the results go to results/ dir

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,14 @@
+from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def generate_plots_flca(csv_file = "flca_results.csv", scalability_time_img="flca_scalability_time.png",
-                        scalability_objective_img="flca_scalability_objective.png",
-                        cost_breakdown_img="flca_cost_breakdown.png"):
+RESULTS_DIR = Path(__file__).parent.parent / "results"
+RESULTS_DIR.mkdir(exist_ok=True)
+
+def generate_plots_flca(csv_file=RESULTS_DIR / "flca_results.csv",
+                        scalability_time_img=RESULTS_DIR / "flca_scalability_time.png",
+                        scalability_objective_img=RESULTS_DIR / "flca_scalability_objective.png",
+                        cost_breakdown_img=RESULTS_DIR / "flca_cost_breakdown.png"):
     df = pd.read_csv(csv_file)
 
     # Plot 1: solve time
@@ -36,10 +41,13 @@ def generate_plots_flca(csv_file = "flca_results.csv", scalability_time_img="flc
     plt.grid(True)
     plt.savefig(cost_breakdown_img)
 
-    print("\nAll instances processed. Results saved to flca_results.csv")
-    print(f"Plot about scalability and cost breakdown saved in flca_scalability_time.png, flca_scalability_objective.png, flca_cost_breakdown.png")
+    print(f"\nAll instances processed. Results saved to {csv_file}")
+    print(f"Plots saved in {scalability_time_img}, {scalability_objective_img}, {cost_breakdown_img}")
 
-def generate_plots_ils(csv_file = "ils_results.csv", scalability_time_img="ils_scalability_time.png", scalability_objective_img="ils_scalability_objective.png", cost_breakdown_img="ils_cost_breakdown.png"):
+def generate_plots_ils(csv_file=RESULTS_DIR / "ils_results.csv",
+                       scalability_time_img=RESULTS_DIR / "ils_scalability_time.png",
+                       scalability_objective_img=RESULTS_DIR / "ils_scalability_objective.png",
+                       cost_breakdown_img=RESULTS_DIR / "ils_cost_breakdown.png"):
     df = pd.read_csv(csv_file)
 
     # Plot 1: solve time
