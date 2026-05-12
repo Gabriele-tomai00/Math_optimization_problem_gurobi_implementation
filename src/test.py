a378998c81b414803552c9cb1ba73f8fff09448b
Gabriele <tomaigabriele@gmail.com>
2026-05-12 12:26:50 +0200

    fix test.py i file vengono ricreati correttamente a ogni run del programma

--- src/test.py
+++ src/test.py
@@ -1,27 +1,17 @@
-import argparse
-import random
-import copy
 import os
-import time
-import csv
-from datetime import datetime
-from zoneinfo import ZoneInfo
-
-import gurobipy as gp
-from gurobipy import GRB
-from data_loader import get_data_from_file_excel, validate_dimensions
-from utils import generate_plots_ils, format_time
 from ils import *
 from run_mibs import *
 
 
 if __name__ == "__main__":
     results_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "test_results")
+    os.makedirs(results_dir, exist_ok=True)
+
     results_ils_file = os.path.join(results_dir, "ils_results.csv")
     if os.path.exists(results_ils_file):
         os.remove(results_ils_file)
 
-    # if you want, you can set a time limit in seconds and 
+    # if you want, you can set a time limit in seconds and
     # you need to add it as last parameter in the function call
     # instance_time_limit = 2 * 3600
 
@@ -33,7 +23,15 @@ if __name__ == "__main__":
 
     ### MIBS
     results_mibs_file = os.path.join(results_dir, "mibs_results.csv")
+    if os.path.exists(results_mibs_file):
+        os.remove(results_mibs_file)
+
     single_mibs_dir = os.path.join(results_dir, "single_mibs")
+    os.makedirs(single_mibs_dir, exist_ok=True)
+    for f in os.listdir(single_mibs_dir):
+        if f.endswith(".txt") or f.endswith(".csv"):
+            os.remove(os.path.join(single_mibs_dir, f))
+
     run_mibs_instance(1, 0, results_dir=single_mibs_dir, output_csv=results_mibs_file)
     run_mibs_instance(2, 0, results_dir=single_mibs_dir, output_csv=results_mibs_file)
 
